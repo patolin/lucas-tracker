899e021b36cce0fadaf9b11cf7557e6e7a652848
Patricio Reinoso <patricio.reinoso.mendoza@gmail.com>
2025-01-27 23:49:48 -0500

    cambio temporal para no instalar bcrypt por lento

--- backend/app/config.py
+++ backend/app/config.py
@@ -1,7 +1,9 @@
 DATABASE = {
-    "HOST": '192.168.68.200',
+    # "HOST": '192.168.68.200',
+    "HOST": '192.168.68.117',
     "USER": 'root',
-    "PASS": "passw0rd",
+    # "PASS": "passw0rd",
+    "PASS": "patolin",
     "PORT": ' 3306',
     "BASE": 'lucas'
 }

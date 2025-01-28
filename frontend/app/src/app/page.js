9b099b0b864e78a1599923f5b74686522baf4533
Patricio Reinoso <patricio.reinoso.mendoza@gmail.com>
2025-01-27 23:25:37 -0500

    filtro de tabla (#4)

--- frontend/app/src/app/page.js
+++ frontend/app/src/app/page.js
@@ -4,6 +4,7 @@ import { useState, useEffect } from 'react';
 import { Tabla } from '@/components/tabla';
 import { Modal } from '@/components/modal';
 import { FormIngreso } from '@/components/formIngreso';
+import { FiltroTabla } from '@/components/filtroTabla';
 
 export default function Home() {
   const [modalOpen, setModalOpen] = useState(false);
@@ -29,14 +30,12 @@ export default function Home() {
     actividades.forEach((item) => {
       objActividades[item.id] = item.nombre;
     });
+
+  const handleFilterResults = (filteredData) => {
+    setData(filteredData);
+  };
   return (
     <div className='container mx-auto p-4'>
-      <button
-        onClick={() => setModalOpen(true)}
-        className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded'
-      >
-        Agregar
-      </button>
       <Modal isOpen={modalOpen} onClose={() => setModalOpen(false)}>
         <h2 className='text-xl font-bold mt-6 mb-4'>Añadir nueva actividad</h2>
         <FormIngreso
@@ -49,7 +48,14 @@ export default function Home() {
           }}
         />
       </Modal>
-      <h2 className='text-2xl font-bold mb-4'>Qué hizo el Lucas?</h2>
+      <h2 className='text-2xl font-bold mb-4'>Qué hizo el Lucas hoy?</h2>
+      <button
+        onClick={() => setModalOpen(true)}
+        className='bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded'
+      >
+        Agregar actividad
+      </button>
+      <FiltroTabla onFilter={handleFilterResults} />
       <Tabla data={data} actividades={objActividades} />
     </div>
   );
